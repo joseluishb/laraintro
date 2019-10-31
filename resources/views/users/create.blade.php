d777b4990e3d23947e107cfaa8e8a0fa349a8587
Jose Luis Huaytalla <joseluishube@gmail.com>
2019-10-31 16:41:29 -0500

    anadiendo boostrap

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,38 +3,41 @@
 @section('title', "Crear Usuario")
 
 @section('content')
-    <h1>Crear nuevo usuario</h1>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <h5>Por favor corrija los errores debajo:</h5>
-           <ul>
-               @foreach($errors->all() as $error)
-                   <li>{{ $error }}</li>
-               @endforeach
-           </ul>
+    <div class="card">
+        <h4 class="card-header">Crear nuevo usuario</div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <h5>Por favor corrija los errores debajo:</h5>
+                   <ul>
+                       @foreach($errors->all() as $error)
+                           <li>{{ $error }}</li>
+                       @endforeach
+                   </ul>
+                </div>
+            @endif
+        
+            <form method="post" action="{{ url('/usuarios') }}">
+                {!! csrf_field() !!}
+        
+                <div class="form-group">
+                    <label for="name">Nombre:</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="ex. Juan Pérez" value="{{ old('name') }}">
+                </div>
+                <div class="form-group">
+                    <label for="email">Correo electrónico</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="jperez@example.com" value="{{ old('email') }}">
+                </div>
+                <div class="form-group">
+                    <label for="password">Contraseña:</label>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="mayor a 6 caracteres">
+                </div>
+                <button type="submit" class="btn btn-primary">Crear usuario</button>
+                <a href="{{ route('users.index') }}" class="btn btn-link">Regresar a usuarios</a>
+        
+            </form>
         </div>
-    @endif
-
-    <form method="post" action="{{ url('/usuarios') }}">
-        {!! csrf_field() !!}
-        <label for="name">Nombre:</label>
-        <input type="text" name="name" id="name" placeholder="ex. Juan Pérez">
-        {{--@if ($errors->has('name'))--}}
-            {{--<p>{{ $errors->first('name') }}</p>--}}
-        {{--@endif--}}
-        <br>
-        <label for="email">Correo electrónico</label>
-        <input type="email" name="email" id="email" placeholder="jperez@example.com" value="{{ old('email') }}">
-        <br>
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" placeholder="mayor a 6 caracteres">
-        <br>
-        <button type="submit">Crear usuario</button>
-    </form>
+    </div>
 
 
-    <p>
-        <a href="{{ route('users.index') }}">Regresar a usuarios</a>
-    </p>
 @endsection
